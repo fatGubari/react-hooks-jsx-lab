494d1c3c01f6ca4fabb85c02f11478c1e8fc95c1
fatima abdullah <fatimajubary@gmail.com>
2023-03-03 09:07:48 +0300

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return(  <div>
+    <h2>About Me</h2>
+    <p>Hellot, World</p>
+    <img src={image} alt="I made this" />
+  </div>)
 }
 
 export default About;
